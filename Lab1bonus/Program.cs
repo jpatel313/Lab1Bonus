5f75e4448eaac6adbec4335cea8c83a800562c37
Jay Patel <jcp313@gmail.com>
2016-10-25 12:32:47 -0400

    first commit

---
+++ Lab1bonus/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1bonus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //convert given number grades into letter grades
+            Console.WriteLine("Welcome to the Letter Grade Converter!");
+
+
+            string prompt;
+            do
+            {
+                Console.WriteLine("Please enter a numerical grader: ");
+
+                string inputGrade = Console.ReadLine();
+
+                int inputInt = Convert.ToInt32(inputGrade);
+
+                if (inputInt >= 90)
+                {
+                    Console.WriteLine("Letter Grade: A");
+                }
+                else if (inputInt >= 80 && inputInt <= 89)
+                {
+                    Console.WriteLine("Letter Grade: B");
+                }
+
+                else if (inputInt >= 70 && inputInt <= 79)
+                {
+                    Console.WriteLine("Letter Grade: C");
+                }
+
+                else if (inputInt >= 60 && inputInt <= 69)
+                {
+                    Console.WriteLine("Letter Grade: D");
+                }
+                else if (inputInt <= 59)
+                {
+                    Console.WriteLine("Letter Grade: F");
+                }
+
+                Console.Write("Do you want to continue?(y/n): ");
+
+                prompt = Console.ReadLine();
+            }
+            while (prompt == "y" || prompt == "Y") ;
+
+        }
+
+
+    }
+}
+
